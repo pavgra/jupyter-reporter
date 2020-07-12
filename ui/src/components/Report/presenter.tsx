a547b022258e83c3f4745daae7f32a4d6fc19120
Pavel Grafkin <pavgra@gmail.com>
2020-07-12 20:24:04 +0300

    Adds loader for report page

--- ui/src/components/Report/presenter.tsx
+++ ui/src/components/Report/presenter.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import {useParams} from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
+import { sendGet } from 'services/Api';
 
 const useStyles = makeStyles((theme) => ({
     header: {
@@ -12,13 +13,34 @@ const useStyles = makeStyles((theme) => ({
 function Report() {
     const classes = useStyles();
     const {name} = useParams();
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true);
+            try {
+                const res = await sendGet(`/api/v1/reports/${name}`);
+                setData(escape(res));
+            } catch (e) {
+                alert(`Failed to load report: ${e}`)
+            }
+            setIsLoading(false);
+        })();
+    }, [name]);
+
     return (
         <>
             <Typography variant="h5" className={classes.header}>
                 {name}
             </Typography>
             <div>
-                <iframe src={`http://localhost:5000/api/v1/reports/${name}`} width="100%" height="850px"></iframe>
+                {isLoading && (
+                    <>Loading...</>
+                )}
+                {!isLoading && (
+                    <iframe src={`data:text/html;charset=utf-8,${data}`} width="100%" height="850px"></iframe>
+                )}
             </div>
         </>
     )
